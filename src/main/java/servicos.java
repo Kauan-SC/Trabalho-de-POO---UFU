20adb953b98ab0595f3ede7d05dd91a2f8b13fb4
Kauan-SC <kauansc12345@gmail.com>
2026-07-19 18:41:11 -0300

    Removendo servicos

--- src/main/java/servicos.java
+++ src/main/java/servicos.java
@@ -1,72 +1,72 @@
-import java.util.ArrayList;
+// import java.util.ArrayList;
 
-import emprestimos.Emprestimo;
-import excecoes.LivroIndisponivelException;
-import excecoes.LivroNaoEncontradoException;
-import excecoes.PessoaNaoEncontradaException;
-import livros.Livro;
-import pessoas.Aluno;
-import pessoas.Pessoa;
+// import emprestimos.Emprestimo;
+// import excecoes.LivroIndisponivelException;
+// import excecoes.LivroNaoEncontradoException;
+// import excecoes.PessoaNaoEncontradaException;
+// import livros.Livro;
+// import pessoas.Aluno;
+// import pessoas.Pessoa;
 
-// Classe responsável por fornecer os serviços essenciais do sistema de biblioteca
-public class servicos {
+// // Classe responsável por fornecer os serviços essenciais do sistema de biblioteca
+// public class servicos {
 
-    // Serviços de busca de livros e pessoas, empréstimos e devoluções
-    public Livro buscarLivro(ArrayList<Livro> livros, int id) throws LivroNaoEncontradoException {
-        for (Livro livro : livros) {
-            if (livro.getId() == id)
-                return livro;
-        }
-        throw new LivroNaoEncontradoException("Livro com ID " + id + " não encontrado.");
-    }
+//     // Serviços de busca de livros e pessoas, empréstimos e devoluções
+//     public Livro buscarLivro(ArrayList<Livro> livros, int id) throws LivroNaoEncontradoException {
+//         for (Livro livro : livros) {
+//             if (livro.getId() == id)
+//                 return livro;
+//         }
+//         throw new LivroNaoEncontradoException("Livro com ID " + id + " não encontrado.");
+//     }
 
-// ------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// // ------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    public Pessoa buscarPessoa(ArrayList<Pessoa> pessoas, int id) throws PessoaNaoEncontradaException {
-        for (Pessoa pessoa : pessoas) {
-            if (pessoa.getId() == id)
-                return pessoa;
-        }
-        throw new PessoaNaoEncontradaException("Pessoa com ID " + id + " não encontrada.");
-    }
+//     public Pessoa buscarPessoa(ArrayList<Pessoa> pessoas, int id) throws PessoaNaoEncontradaException {
+//         for (Pessoa pessoa : pessoas) {
+//             if (pessoa.getId() == id)
+//                 return pessoa;
+//         }
+//         throw new PessoaNaoEncontradaException("Pessoa com ID " + id + " não encontrada.");
+//     }
 
-// ------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// // ------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    public void emprestarLivro(ArrayList<Livro> livros, ArrayList<Pessoa> pessoas, ArrayList<Emprestimo> emprestimos,
-            int idLivro, int idPessoa)
-            throws LivroNaoEncontradoException, LivroIndisponivelException, PessoaNaoEncontradaException {
+//     public void emprestarLivro(ArrayList<Livro> livros, ArrayList<Pessoa> pessoas, ArrayList<Emprestimo> emprestimos,
+//             int idLivro, int idPessoa)
+//             throws LivroNaoEncontradoException, LivroIndisponivelException, PessoaNaoEncontradaException {
 
-        Livro livro = buscarLivro(livros, idLivro);
-        Pessoa pessoa = buscarPessoa(pessoas, idPessoa);
+//         Livro livro = buscarLivro(livros, idLivro);
+//         Pessoa pessoa = buscarPessoa(pessoas, idPessoa);
 
-        if (!livro.isDisponibilidade()) {
-            throw new LivroIndisponivelException("O livro '" + livro.getTitulo() + "' não está disponível.");
-        }
+//         if (!livro.isDisponibilidade()) {
+//             throw new LivroIndisponivelException("O livro '" + livro.getTitulo() + "' não está disponível.");
+//         }
 
-        livro.setDisponibilidade(false);
-        Emprestimo emprestimo = new Emprestimo(livro, pessoa);
-        emprestimos.add(emprestimo);
+//         livro.setDisponibilidade(false);
+//         Emprestimo emprestimo = new Emprestimo(livro, pessoa);
+//         emprestimos.add(emprestimo);
 
-        if (pessoa instanceof Aluno aluno) {
-            aluno.getEmprestimosAtivos().add(emprestimo);
-        }
-    }
+//         if (pessoa instanceof Aluno aluno) {
+//             aluno.getEmprestimosAtivos().add(emprestimo);
+//         }
+//     }
   
-// ------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// // ------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    public void devolverLivro(ArrayList<Livro> livros, ArrayList<Emprestimo> emprestimos, int idLivro)
-            throws LivroNaoEncontradoException {
+//     public void devolverLivro(ArrayList<Livro> livros, ArrayList<Emprestimo> emprestimos, int idLivro)
+//             throws LivroNaoEncontradoException {
 
-        Livro livro = buscarLivro(livros, idLivro);
-        for (Emprestimo emprestimo : emprestimos) {
-            if (emprestimo.getLivro() == livro && emprestimo.isAtivo()) {
-                emprestimo.registrarDevolucao();
-                livro.setDisponibilidade(true);
-                if (emprestimo.getPessoa() instanceof Aluno aluno) {
-                    aluno.getEmprestimosAtivos().remove(emprestimo);
-                }
-                return;
-            }
-        }
-    }
-}
+//         Livro livro = buscarLivro(livros, idLivro);
+//         for (Emprestimo emprestimo : emprestimos) {
+//             if (emprestimo.getLivro() == livro && emprestimo.isAtivo()) {
+//                 emprestimo.registrarDevolucao();
+//                 livro.setDisponibilidade(true);
+//                 if (emprestimo.getPessoa() instanceof Aluno aluno) {
+//                     aluno.getEmprestimosAtivos().remove(emprestimo);
+//                 }
+//                 return;
+//             }
+//         }
+//     }
+// }
